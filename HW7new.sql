7c71116200909b8abe8715445b2bbbe722a305f5
proslava <proslava@mail.ru>
2019-10-14 12:48:29 +0300

    New HW7

--- HW7new.sql
+++ HW7new.sql
@@ -6,9 +6,13 @@ select users.id, users.name, users.birthday_at, orders.id as 'order_id', orders.
 from users
 join orders on users.id = orders.user_id;
 
+select
+	users.id,
+	users.name,
+	count(*) as total_orders
+from users
+inner join orders on users.id = orders.user_id
+group by orders.user_id
+order by total_orders desc
+
 -- Выведите список товаров products и разделов catalogs, который соответствует товару.
-
-
-select *
-from products, catalogs
-where products.id = catalog_id
